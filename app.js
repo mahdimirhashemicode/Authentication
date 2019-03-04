2fc988e4d0f6447a3aa879d6a906e7abf4c7381a
Mahdi <mahdi.mirhashemi.code@gmail.com>
2019-03-03 16:34:17 -0800

    add new encryption method with hashing and salting using bcrypt

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require('bcrypt');
+
 
 const app = express();
 
@@ -16,6 +17,8 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+const saltRounds = 10;
 
 const userSchema = new mongoose.Schema({
 	email: String,
@@ -39,34 +42,38 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-	const newUser = new User({
-		email: req.body.username,
-		password: md5(req.body.password)
-	});
-	newUser.save((err) => {
-		if(!err) {
-			res.render("secrets");
-		} else {
-			res.send(err);
-		}
-	});
 
+	bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+		const newUser = new User({
+				email: req.body.username,
+				password: hash
+			});
+			newUser.save((err) => {
+				if(!err) {
+					res.render("secrets");
+				} else {
+					res.send(err);
+				}
+			});
+		});
 });
 
 app.post("/login", (req,res) => {
 	const username= req.body.username;
-	const password= md5(req.body.password);
+	const password= req.body.password;
 
 	User.findOne({email: username}, (err, foundUser) => {
 		if(err) {
 			console.log(err);
 		} else {
 			if(foundUser) {
-				if(foundUser.password === password) {
-					res.render("secrets");
-				} else {
-					res.send("Sorry! Email and Password don't match");
-				}
+				bcrypt.compare(password, foundUser.password, (err, result) => {
+					if(result === true) {						
+						res.render("secrets");
+					} else {
+						res.send("Sorry! Email and Password don't match");
+					}
+				});			
 			}
 		}
 	});
