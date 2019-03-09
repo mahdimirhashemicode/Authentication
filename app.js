58103e315c0a86d2a3af0bdc9e561865a397db56
Mahdi <mahdi.mirhashemi.code@gmail.com>
2019-03-09 13:19:38 -0800

    add submit post method and modify secret page

--- app.js
+++ app.js
@@ -32,7 +32,8 @@ mongoose.set("useCreateIndex", true);
 const userSchema = new mongoose.Schema({
 	email: String,
 	password: String,
-	googleId: String
+	googleId: String,
+	secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -89,11 +90,43 @@ app.get("/register", (req, res) => {
 });
 
 app.get("/secrets", (req, res) => {
+	User.find({"secret": {$ne:null}}, (err, foundUsers) => {
+		if(err) {
+			console.log(err);
+		} else {
+			if(foundUsers) {
+				res.render("secrets", {usersWithSecrets: foundUsers});
+			}
+		}
+	});
+});
+
+
+app.get("/submit", (req, res) => {
 	if(req.isAuthenticated()) {
-		res.render("secrets");
+		res.render("submit");
 	} else {
 		res.redirect("/login");
-	}
+	}	
+});
+
+
+app.post("/submit", (req, res) => {
+	const submittedSecret = req.body.secret;
+
+	User.findById(req.user.id, (err, foundUser) => {
+		if(err) {
+			console.log(err);
+		} else {
+			if(foundUser) {
+				foundUser.secret = submittedSecret;
+				foundUser.save(() => {
+					res.redirect("/secrets");
+				});
+			}
+		}
+	});
+
 });
 
 app.get("/logout", (req, res) => {
